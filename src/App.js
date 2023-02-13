50a466a5a3c8a2fb445bc9dc18094ba8f28d244c
Guilherme Tavares <guilherme.alves@creditas.com>
2023-02-12 23:38:48 -0300

    feat: created structure and read/insert firestore data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { collection, getDocs, updateDoc, doc } from "firebase/firestore"; 
+import { db } from './firebaseUtils'
 
 function App() {
+
+  const [documentInfos, setDocumentInfos] = useState([])
+  const [persons, setPersons] = useState([])
+  const [selectPerson, setSelectPerson] = useState("")
+
+  const handleClickRegister = async () => {
+    console.log("teste")
+    await updateDoc(doc(db, "person", selectPerson), {
+      lastTime: new Date(),
+      times: persons.find(element => element.id === selectPerson).times+1
+    })
+  }
+
+  const handleSelectUser = (id) => {
+    setSelectPerson(id)
+  }
+
+  useEffect(() => {
+    const getDocumentInfos = async() => {
+      const responsePersons = await getDocs(collection(db, "person"));
+      setDocumentInfos(responsePersons)
+    };
+    getDocumentInfos()
+  },[]);
+
+  useEffect(() => {
+    console.log(documentInfos)
+    documentInfos?.forEach((doc) => {
+      console.log(doc.data());
+     setPersons(persons => [...persons, doc.data()] )
+    })
+  }, [documentInfos])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {persons && (
+        <select name="persons" onChange={event => handleSelectUser(event.target.value)}>
+          {persons?.map(({name, id}, index) => (
+            <option key={name+index} value={id}>{name}</option>
+          ))}
+        </select>
+      )} 
+      <input type="button" onClick={handleClickRegister} value="Registrar treino"/>
     </div>
   );
 }
---
+++ src/firebaseUtils.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf9kg4Xn4k5PKvEUXhtWNunDMWPFmNXls",
+  authDomain: "who-went-to-gym.firebaseapp.com",
+  projectId: "who-went-to-gym",
+  storageBucket: "who-went-to-gym.appspot.com",
+  messagingSenderId: "767351817771",
+  appId: "1:767351817771:web:00da844b1fcb6beae48c6e",
+  measurementId: "G-PD8EYN2V6K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { analytics, db };
