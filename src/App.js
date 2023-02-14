046b9128193c7e204fbfa846674d8c1dfc4e23db
Guilherme Tavares <guilherme.alves@creditas.com>
2023-02-13 23:24:59 -0300

    feat: added empty date validation

--- src/App.js
+++ src/App.js
@@ -1,56 +1,85 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { collection, getDocs, updateDoc, doc } from "firebase/firestore"; 
-import { db } from './firebaseUtils'
-import { isSameDate } from './utils/isSameDate';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { collection, getDocs, updateDoc, doc } from "firebase/firestore";
+import { db } from "./firebaseUtils";
+import { isSameDate } from "./utils/isSameDate";
 
 function App() {
-
-  const [documentInfos, setDocumentInfos] = useState([])
-  const [persons, setPersons] = useState([])
-  const [selectPerson, setSelectPerson] = useState("")
+  const [documentInfos, setDocumentInfos] = useState([]);
+  const [persons, setPersons] = useState([]);
+  const [selectPerson, setSelectPerson] = useState("");
 
   const handleClickRegister = async () => {
-    if(isSameDate(new Date(persons.find(element => element.id === selectPerson).lastTime.seconds*1000), new Date())) {
-      alert(`${persons.find(element => element.id === selectPerson).name} já registrou um treino hoje`)
-    } else {
+    if (
+      isSameDate(
+        new Date(
+          persons.find((element) => element.id === selectPerson)?.lastTime
+            .seconds * 1000
+        ),
+        new Date()
+      ) &&
+      selectPerson
+    ) {
+      alert(
+        `${
+          persons.find((element) => element.id === selectPerson).name
+        } já registrou um treino hoje`
+      );
+      window.location.reload();
+    } else if (selectPerson) {
       await updateDoc(doc(db, "person", selectPerson), {
         lastTime: new Date(),
-        times: persons.find(element => element.id === selectPerson).times+1
-      })
+        times: persons.find((element) => element.id === selectPerson).times + 1,
+      });
+      alert(
+        `${
+          persons.find((element) => element.id === selectPerson).name
+        }, o seu treino foi registrado com sucesso.`
+      );
+      window.location.reload();
+    } else if (!selectPerson) {
+      alert("Selecione uma pessoa para realizar a marcação");
     }
-  }
+  };
 
   const handleSelectUser = (id) => {
-    setSelectPerson(id)
-  }
+    setSelectPerson(id);
+  };
 
   useEffect(() => {
-    const getDocumentInfos = async() => {
+    const getDocumentInfos = async () => {
       const responsePersons = await getDocs(collection(db, "person"));
-      setDocumentInfos(responsePersons)
+      setDocumentInfos(responsePersons);
     };
-    getDocumentInfos()
-  },[]);
+    getDocumentInfos();
+  }, []);
 
   useEffect(() => {
     documentInfos?.forEach((doc) => {
-      console.log(doc.data());
-     setPersons(persons => [...persons, doc.data()] )
-    })
-  }, [documentInfos])
-
+      setPersons((persons) => [...persons, doc.data()]);
+    });
+  }, [documentInfos]);
 
   return (
     <div className="App">
       {persons && (
-        <select name="persons" onChange={event => handleSelectUser(event.target.value)}>
-          {persons?.map(({name, id}, index) => (
-            <option key={name+index} value={id}>{name}</option>
+        <select
+          name="persons"
+          onChange={(event) => handleSelectUser(event.target.value)}
+        >
+          <option value=""></option>
+          {persons?.map(({ name, id }, index) => (
+            <option key={name + index} value={id}>
+              {name}
+            </option>
           ))}
         </select>
-      )} 
-      <input type="button" onClick={handleClickRegister} value="Registrar treino"/>
+      )}
+      <input
+        type="button"
+        onClick={handleClickRegister}
+        value="Registrar treino"
+      />
     </div>
   );
 }
