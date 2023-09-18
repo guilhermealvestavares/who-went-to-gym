413a1892f1964f24930c87217957fc0c4e0cb81c
Guilherme Tavares <guilhermetavares@MacBook-Air-de-Guilherme.local>
2023-09-17 22:28:33 -0300

    fix: removed warnings

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { collection, getDocs, updateDoc, doc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebaseUtils";
 import {
   Wrapper,
@@ -15,6 +15,7 @@ export const Ranking = () => {
   const [persons, setPersons] = useState([]);
   const [sortedPersons, setSortedPersons] = useState([]);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const sortPersons = () => {
     setSortedPersons(persons.sort((prev, next) => next.times - prev.times));
   };
