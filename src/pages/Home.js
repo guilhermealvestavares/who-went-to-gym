d04ed0029f78277d9e3f384770df504a4e74e957
Guilherme Tavares <guilhermetavares@MacBook-Air-de-Guilherme.local>
2024-01-04 00:59:34 -0300

    test: testing reloads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -57,7 +57,6 @@ export const Home = () => {
           },
           { merge: true }
         );
-        window.location.reload(true);
       })
       .catch((error) => console.log(error));
   };
